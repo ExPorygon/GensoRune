3e3702fdbfdc421a45606fa49223cc3446442cd0
ExPorygon <ozzy840@gmail.com>
2022-01-16 14:46:37 -0500

    Revert "Implement basic console logging system for combat"
    This reverts commit 824c144b71b2fae480dae48847b93cf23da6a3ad.

--- TurnQueue.gd
+++ TurnQueue.gd
@@ -18,6 +18,7 @@ func _process(delta):
 func play_turn():
 	var target: Battler
 	var action: CombatAction
+	print("Turn Start")
 	active_char.isGuarding = false
 	action = yield(active_char.ai.choose_action(active_char), "completed")
 	target = yield(active_char.ai.choose_target(active_char,action), "completed")
@@ -26,4 +27,5 @@ func play_turn():
 	
 	var new_index : int = (active_char.get_index()+1) % get_child_count()
 	active_char = get_child(new_index)
+	print("Turn End")
 	play_turn()
