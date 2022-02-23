e10b8485540fa0ed184854877502755d06c3ad19
ExPorygon <ozzy840@gmail.com>
2022-02-22 20:57:59 -0500

    Add accuracy property to SpellData

--- SpellData.gd
+++ SpellData.gd
@@ -18,6 +18,7 @@ export var name: String
 
 export var base_damage: int
 export var mana_cost: int
+export(float, 0.0, 1.0) var accuracy: float
 
 export(Target) var target_mode
 export(Group) var group_mode
