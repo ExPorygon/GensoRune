d7366876c62d0aa0ace5022726482ad89e52cbbb
Luna <ozzy840@gmail.com>
2023-02-15 19:46:33 -0500

    Removed ignored folder from repo

--- Attack.gd
+++ Attack.gd
@@ -1,9 +1,10 @@
 extends CombatAction
 
 var action_name = "Attack"
+target_mode = 0
 
 func execute(target):
 	var damage: int
 	damage = actor.stats.attack - target.stats.defense
-	target.damage(self,damage)
+	target.damage(actor,damage)
 	#battle_log_text.emit(str(actor.name) + " attacks! " + str(target.name) + " takes " + str(damage) + " damage!")
